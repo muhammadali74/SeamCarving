f92527373dbc6f05a65e46f45489a9993da30de7
muhammadali74 <sn07590@st.habib.edu.pk>
2024-04-29 13:07:53 +0500

    timeinit

---
+++ timing.py
@@ -0,0 +1,18 @@
+import seamcarving
+import greedy
+import dynamic_gpu
+import bruteforce
+import time
+import timeit
+
+
+
+
+# define a function for timing plot of one image at a different scales for each algorithm. should take image file as parameter
+
+# define a function for timing plof of images of different sizes at a fixed scale s for each algorithm. should take scale as input parameter
+# this also need to have images available for different sizes
+# do for n*n images from internet may be some webiste which gives same image of multiple pixel sizes
+
+
+
