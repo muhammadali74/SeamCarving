b1c2d84756497c52faaf03d95df53620ed9df87c
Muhammad Ali <sn07590@st.habib.edu.pk>
2024-04-30 09:10:03 +0500

    t

--- timing.py
+++ timing.py
@@ -8,13 +8,15 @@ import timeit
 
 
 
-# define a function for timing overall plot of one image at a different scales for each algorithm. should take image file as parameter
+# define a function for timing overall plot of different size imagea at a different scales for each algorithm. should take image file as parameter
 
-# define a function for timing plot seam acrving (minimumseam) algo only of one image at a different scales for each algorithm. should take image file as parameter
-
-# define a function for timing plof of images of different sizes at a fixed scale s for each algorithm. should take scale as input parameter
+# define a function for timing overall plot of images of different sizes at a fixed scale s for each algorithm. should take scale as input parameter
 # this also need to have images available for different sizes
 # do for n*n images from internet may be some webiste which gives same image of multiple pixel sizes
 
+
+# define a function for timing plot of seamcarving algo (minimin seam) only of images of different sizes at a fixed scale s for each algorithm. should take scale as input parameter
+# this also need to have images available for different sizes
+# do for n*n images from internet may be some webiste which gives same image of multiple pixel sizes
 
 
